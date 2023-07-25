aef976aba09992e8d17e388bfb5875a0f7edaf41
Thiago Felipe Dos Santos <thiagonfss@gmail.com>
2023-07-25 09:33:48 -0300

    Alteração em controller para retornar status code corretamente

--- servico-curso/Controller/CursosController.cs
+++ servico-curso/Controller/CursosController.cs
@@ -1,7 +1,10 @@
 ﻿
 
 using servico_curso.Model;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -15,19 +18,44 @@ namespace Servico_Curso.Controller
         /// <param name="ativo">Parâmetro boleano opcional que caso seja passado filtra a lista de cursos ativos ou inativos.</param>
         /// <returns></returns>
         [HttpGet]
-        public Task<List<CursoModel>> BuscarCursos(bool? ativo = null)
+        public async Task<HttpResponseMessage> BuscarCursos(bool? ativo = null)
         {
-            return CursoModel.BuscarCursos(ativo);
+            try
+            {
+                List<CursoModel> cursos = await CursoModel.BuscarCursos(ativo);
+                if (cursos == null && cursos.Count == 0)
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                
+                return Request.CreateResponse(HttpStatusCode.OK, cursos);
+                
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
         }
+
         /// <summary>
         /// Busca as informações de um curso com base no Id informado
         /// </summary>
         /// <param name="id">Parâmetro obrigatório a ser informado para realizar a busca por Id.</param>
         /// <returns></returns>
         [HttpGet]
-        public Task<CursoModel> BuscarCurso(int id)
+        public async Task<HttpResponseMessage> BuscarCurso(int id)
         {
-            return CursoModel.BuscarCurso(id);
+            try
+            {
+                CursoModel curso = await CursoModel.BuscarCurso(id);
+                
+                if (curso == null)
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                
+                return Request.CreateResponse(HttpStatusCode.OK, curso);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
         }
     }
 }
