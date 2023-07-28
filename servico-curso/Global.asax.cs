f274e0950af81aa0887186393376c5436fcd76e5
Thiago Felipe Dos Santos <thiagonfss@gmail.com>
2023-07-28 02:19:11 -0300

    Akteração para CORS

--- servico-curso/Global.asax.cs
+++ servico-curso/Global.asax.cs
@@ -20,5 +20,19 @@ namespace servico_curso
             GlobalConfiguration.Configure(WebApiConfig.Register);
 
         }
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        { // Habilitar o CORS para todas as origens, métodos e cabeçalhos
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+
+            // Permitir que os cookies sejam enviados nas solicitações de origem cruzada (opcional)
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Credentials", "true");
+            // Se for uma requisição OPTIONS, definir um código de status de sucesso (200) e encerrar a resposta
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.StatusCode = 200; HttpContext.Current.Response.End();
+            }
+        }
     }
 }
